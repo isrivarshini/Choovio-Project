juandesant/mainflux

Add coap support
Signed-off-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

coap/nats/messages.go
@@ -1,0 +1,27 @@
+// Package nats contains NATS-specific message repository implementation.
+package nats
+
+import (
+	"encoding/json"
+
+	"github.com/mainflux/mainflux/normalizer"
+	broker "github.com/nats-io/go-nats"
+)
+
+const topic string = "adapter.coap"
+
+// Stored NATS connection
+var snc *broker.Conn
+
+func StoreConnection(nc *broker.Conn) {
+	snc = nc
+}
+
+func Send(msg normalizer.Message) error {
+	b, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+
+	return snc.Publish(topic, b)
+}

isrivarshini/Choovio-Project

coap/nats/messages.go
@@ -1,0 +1,27 @@
+// Package nats contains NATS-specific message repository implementation.
+package nats
+
+import (
+	"encoding/json"
+
+	"github.com/mainflux/mainflux/writer"
+	broker "github.com/nats-io/go-nats"
+)
+
+const topic string = "msg.coap"
+
+// Stored NATS connection
+var snc *broker.Conn
+
+func StoreConnection(nc *broker.Conn) {
+	snc = nc
+}
+
+func Send(msg writer.RawMessage) error {
+	b, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+
+	return snc.Publish(topic, b)
+}